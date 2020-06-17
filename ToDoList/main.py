5a42f84e2e8694e5a794029ac89df61f1a8c80af
Nic-T <67067741+Nic-T@users.noreply.github.com>
2020-06-18 00:31:34 +0300

    First GUI app
    A normal window with name and resolution. It contains a frame. Next I want to make the frame to resize itself  depending on the resolution of the window

---
+++ ToDoList/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def main():
+
+
+    root = Tk()
+    root.title("To Do List")
+    root.geometry("1920x1080")
+
+    Menu1= Frame(root,height="1080", width="200", bg="grey")
+    Menu1.pack(side =LEFT)
+
+
+
+    root.mainloop()
