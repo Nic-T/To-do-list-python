71debb643eac76ed46b75c41e209f4d5d7522a75
TheNicMic <nicmic8352@gmail.com>
2020-06-21 02:13:37 +0300

    Added front end menu

--- ToDoList/main.py
+++ ToDoList/main.py
@@ -7,9 +7,17 @@ def main():
     root.title("To Do List")
     root.geometry("1920x1080")
 
-    Menu1= Frame(root,height="1080", width="200", bg="grey")
-    Menu1.pack(side =LEFT)
+    Menu1= Frame(root, bg="grey")
+    Menu1.place(relwidth=0.15 , relheight=1,relx=0,rely=0)
 
+    addTask = Button(Menu1,bg='#37bf60',text='Add Task',font="Bahnschrift 25",fg='#5eefff',bd=1)
+    addTask.place(relwidth=1,relheight=0.15,relx=0,rely=0 )
 
+    completedTasks =Button(Menu1, bg='#37bf60', text='Completed Tasks' ,font="Bahnschrift 25",fg='#5eefff',bd=1)
+    completedTasks.place(relwidth=1,relheight=0.15,relx=0,rely=0.15)
+    
+    viewTasks = Button(Menu1,bg='#37bf60',text='View Tasks' ,font="Bahnschrift 25",fg='#5eefff',bd=1)
+    viewTasks.place(relwidth=1,relheight=0.15,relx=0,rely=0.30)
+   
 
     root.mainloop()
