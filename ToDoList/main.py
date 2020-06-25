95a318063584e2403404aea3a43b252e9f1e3f08
Nic-T <nicolastantau2003@gmail.com>
2020-06-26 02:03:45 +0300

    changed main.py layout

--- ToDoList/main.py
+++ ToDoList/main.py
@@ -10,19 +10,19 @@ def main():
     root.geometry("1920x1080")
 
     Menu1= Frame(root, bg="grey")
-    Menu1.place(relwidth=0.15 , relheight=1,relx=0,rely=0)
+    Menu1.place(relwidth=0.20 , relheight=1,relx=0,rely=0)
 
-    addTask = Button(Menu1,bg='#37bf60',text='Add Task',font="Bahnschrift 20",fg='#5eefff',bd=1,command= lambda: AddTask())
-    addTask.place(relwidth=1,relheight=0.15,relx=0,rely=0 )
+    addTask = Button(Menu1,bg='#37bf60',text='Add Task',font="Bahnschrift 20",fg='#5eefff',bd=1,command= lambda: AddTask(), justify=CENTER)
+    addTask.place(relwidth=1,relheight=0.20,relx=0,rely=0)
 
-    completedTasks =Button(Menu1, bg='#37bf60', text='Completed Tasks' ,font="Bahnschrift 20",fg='#5eefff',bd=1)
-    completedTasks.place(relwidth=1,relheight=0.15,relx=0,rely=0.15)
+    completedTasks =Button(Menu1, bg='#37bf60', text='Completed Tasks' ,font="Bahnschrift 20",fg='#5eefff',bd=1, justify=CENTER)
+    completedTasks.place(relwidth=1,relheight=0.20,relx=0,rely=0.20)
     
-    viewTasks = Button(Menu1,bg='#37bf60',text='View Tasks' ,font="Bahnschrift 20",fg='#5eefff',bd=1)
-    viewTasks.place(relwidth=1,relheight=0.15,relx=0,rely=0.30)
+    viewTasks = Button(Menu1,bg='#37bf60',text='View Tasks' ,font="Bahnschrift 20",fg='#5eefff',bd=1, justify=CENTER)
+    viewTasks.place(relwidth=1,relheight=0.20,relx=0,rely=0.40)
    
     space = Message(root, bg= "brown")
-    space.place(relwidth=0.85,relheight=1,relx=0.15,rely=0)
+    space.place(relwidth=0.80,relheight=1,relx=0.20,rely=0)
     
 
     root.mainloop()
